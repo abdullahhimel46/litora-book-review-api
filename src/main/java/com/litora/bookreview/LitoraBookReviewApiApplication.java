24ff97e581ff3062b43482a29d98b1a1c6aad51c
Abdullah Himel <abdullahhimel46@gmail.com>
2026-08-13 17:28:01 +0600

    feat: initialize Spring Boot project structure with Gradle wrapper and postgres

---
+++ src/main/java/com/litora/bookreview/LitoraBookReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.litora.bookreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitoraBookReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitoraBookReviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/litora/bookreview/LitoraBookReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litora.bookreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitoraBookReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
