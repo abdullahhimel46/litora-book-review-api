dd09ec9eb26ce73aeafbcb83e522914469d0d5b9
Abdullah Himel <abdullahhimel46@gmail.com>
2026-08-16 17:26:24 +0600

    feat: implement BookService methods to align with DTO

--- src/main/java/com/litora/bookreview/service/implementation/BookServiceImpl.java
+++ src/main/java/com/litora/bookreview/service/implementation/BookServiceImpl.java
@@ -1,5 +1,8 @@
 package com.litora.bookreview.service.implementation;
 
+import com.litora.bookreview.dto.BookRequest;
+import com.litora.bookreview.dto.BookResponse;
+import com.litora.bookreview.mapper.BookMapper;
 import com.litora.bookreview.model.Book;
 import com.litora.bookreview.repository.BookRepository;
 import com.litora.bookreview.service.BookService;
@@ -7,7 +10,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -16,64 +18,56 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
     @Override
-    public Book createBook(Book book) {
+    public BookResponse createBook(BookRequest request) {
         // check if the book already exists in the database by title and author
         boolean existingBook = bookRepository.existsByTitleAndAuthor(
-                book.getTitle(), book.getAuthor()
-        );
+                request.title(), request.author());
         if (existingBook) {
-            throw new RuntimeException("Book already exists with title: " + book.getTitle() + " and author: " + book.getAuthor());
+            throw new RuntimeException(
+                    "Book already exists with title: " + request.title() + " and author: " + request.author());
         }
 
-        return bookRepository.save(book);
+        Book book = BookMapper.toEntity(request);
+        Book savedBook = bookRepository.save(book);
 
+        return BookMapper.toResponse(savedBook);
     }
 
     @Override
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public List<BookResponse> getAllBooks() {
+        return bookRepository.findAll().
+                stream().
+                map(BookMapper::toResponse).
+                toList();
     }
 
     @Override
-    public Book getBookById(Long id) {
-//        Optional<Book> book = bookRepository.findById(id);
-//        if (book.isPresent()) {
-//            return book.get();
-//        }else {
-//            throw new RuntimeException("Book not found with id: " + id);
-//        }
-        return bookRepository.findById(id).
-                orElseThrow(() -> new RuntimeException
-                        ("Book not found with id: " + id)
-                );
+    public BookResponse getBookById(Long id) {
+        Book book = bookRepository.findById(id).
+                orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+
+        return BookMapper.toResponse(book);
     }
 
     @Override
-    public Book updateBook(Long id, Book book) {
-        Optional<Book> existingBook = bookRepository.findById(id);
+    public BookResponse updateBook(Long id, BookRequest book) {
+        Book existingBook = bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
 
-        if (!existingBook.isPresent()) {
-            throw new RuntimeException("Book not found with id: " + id);
-        }
+        BookMapper.updateEntity(existingBook, book);
 
-        Book surelyExistingBook = existingBook.get();
-
-        surelyExistingBook.setTitle(book.getTitle());
-        surelyExistingBook.setAuthor(book.getAuthor());
-        surelyExistingBook.setGenre(book.getGenre());
-        surelyExistingBook.setPublishedYear(book.getPublishedYear());
-
-        return bookRepository.save(surelyExistingBook);
+        return BookMapper.toResponse(bookRepository.save(existingBook));
     }
 
     @Override
-    public void deleteBook(Long id) {
-        Optional<Book> existingBook = bookRepository.findById(id);
+    public String deleteBook(Long id) {
+        boolean exists = bookRepository.existsById(id);
 
-        if (!existingBook.isPresent()) {
+        if (!exists) {
             throw new RuntimeException("Book not found with id: " + id);
         }
 
         bookRepository.deleteById(id);
+        return "Book deleted successfully with id: " + id;
     }
 }
