e2e98954c49082d18ed48db83d114c50dd98dcc9
Abdullah Himel <abdullahhimel46@gmail.com>
2026-08-16 16:55:06 +0600

    feat: updated BookController logic

--- src/main/java/com/litora/bookreview/controller/BookController.java
+++ src/main/java/com/litora/bookreview/controller/BookController.java
@@ -1,9 +1,12 @@
 package com.litora.bookreview.controller;
 
 
+import com.litora.bookreview.dto.BookRequest;
+import com.litora.bookreview.dto.BookResponse;
 import com.litora.bookreview.model.Book;
 import com.litora.bookreview.service.BookService;
 import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,37 +14,38 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/v1/books")
+@Tag(name = "Book management API", description = "Endpoints for managing books using Spring JDBC")
 public class BookController {
     private final BookService bookService;
 
     // create a new book
     @PostMapping
-    public Book createBook(@RequestBody Book book) {
+    public BookResponse createBook(@RequestBody BookRequest book) {
         return bookService.createBook(book);
     }
 
     // get all books
     @GetMapping()
-    public List<Book> getAllBooks() {
+    public List<BookResponse> getAllBooks() {
         return bookService.getAllBooks();
     }
 
     // get a book by id
     @GetMapping("/{id}")
-    public Book getBookById(@PathVariable Long id) {
+    public BookResponse getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
     // update a book by id
     @PutMapping("/{id}")
-    public  Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+    public BookResponse updateBook(@PathVariable Long id, @RequestBody BookRequest book) {
         return bookService.updateBook(id, book);
     }
 
     // delete a book by id
     @DeleteMapping("/{id}")
-    public void deleteBook(@PathVariable Long id) {
-        bookService.deleteBook(id);
+    public String deleteBook(@PathVariable Long id) {
+        return bookService.deleteBook(id);
     }
 
 
