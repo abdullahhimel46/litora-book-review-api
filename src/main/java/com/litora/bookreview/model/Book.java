04c6e85ce649930181654c0b382fe214e920d291
Abdullah Himel <abdullahhimel46@gmail.com>
2026-08-16 10:45:34 +0600

    feat: updated appplication.yaml and created Book model

---
+++ src/main/java/com/litora/bookreview/model/Book.java
@@ -0,0 +1,27 @@
+package com.litora.bookreview.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private Integer publishedYear;
+
+}
