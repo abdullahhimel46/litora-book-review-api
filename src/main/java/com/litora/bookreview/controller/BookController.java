c0286ae90d4767d9aa8d1004d38c643095fddcf5
Abdullah Himel <abdullahhimel46@gmail.com>
2026-08-18 14:45:20 +0600

    docs(controller): add Swagger @Operation summaries to BookController endpoints

--- src/main/java/com/litora/bookreview/controller/BookController.java
+++ src/main/java/com/litora/bookreview/controller/BookController.java
@@ -5,6 +5,7 @@ import com.litora.bookreview.dto.BookRequest;
 import com.litora.bookreview.dto.BookResponse;
 import com.litora.bookreview.model.Book;
 import com.litora.bookreview.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +21,35 @@ public class BookController {
 
     // create a new book
     @PostMapping
+    @Operation(summary = "Create a new book")
     public BookResponse createBook(@RequestBody BookRequest book) {
         return bookService.createBook(book);
     }
 
     // get all books
     @GetMapping()
+    @Operation(summary = "Get all books")
     public List<BookResponse> getAllBooks() {
         return bookService.getAllBooks();
     }
 
     // get a book by id
     @GetMapping("/{id}")
+    @Operation(summary = "Get a book by id")
     public BookResponse getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
     // update a book by id
     @PutMapping("/{id}")
+    @Operation(summary = "Update a book by id")
     public BookResponse updateBook(@PathVariable Long id, @RequestBody BookRequest book) {
         return bookService.updateBook(id, book);
     }
 
     // delete a book by id
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a book by id")
     public String deleteBook(@PathVariable Long id) {
         return bookService.deleteBook(id);
     }
